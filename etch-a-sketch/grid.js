fefd000d3200441f4d606cc4b2b5e0d8baa7f41c
Santiago Forero <sacostaf.99@outlook.com>
2022-10-12 12:51:43 -0500

    Add function to empty container, and button that allows to change grid size

--- etch-a-sketch/grid.js
+++ etch-a-sketch/grid.js
@@ -1,23 +1,40 @@
 const container = document.querySelector('#container');
 const divs = document.querySelectorAll('.sons');
-for (let i = 0; i < 16; i++) {
-    let columns = document.createElement('div');
-    columns.classList.toggle('divs');
-    container.appendChild(columns);
-    for (let i = 0; i < 16; i++) {
-        let rows = document.createElement('div');
-        rows.classList.toggle('sons');
-        columns.appendChild(rows);
-        rows.addEventListener('mouseover', () => {
-            let randomHex = Math.floor(Math.random()*0xFFFFFF);
-            randomHex = randomHex.toString(16);
-            let randomColor = randomHex.padStart(6,0);
-            randomColor = randomColor.toUpperCase();
-            rows.classList.toggle('hover');
-            
-        });
+const newDivs = document.querySelector('#grid-divs');
+let gridValue = 16;
 
-    }
+function changeColor(gridValue) {
+    for (let i = 0; i < gridValue; i++) {
+        let columns = document.createElement('div');
+        columns.classList.toggle('divs');
+        container.appendChild(columns);
+        for (let j = 0; j < gridValue; j++) {
+            let rows = document.createElement('div');
+            rows.classList.toggle('sons');
+            columns.appendChild(rows);
+            rows.addEventListener('mouseover', () => {
+                let randomHex = Math.floor(Math.random()*0xFFFFFF);
+                randomHex = randomHex.toString(16);
+                let randomColor = randomHex.padStart(6,0);
+                randomColor = randomColor.toUpperCase();
+                rows.classList.toggle('hover');
+                rows.style.background = '#'+randomColor;
+            });
+        }
+    }    
 }
+function cleanContainer(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);        
+    }
+  }
+newDivs.addEventListener('click',() => {
+    cleanContainer(container);
+    gridValue = parseInt(prompt('change grid size to: '));
+    changeColor(gridValue);
+    return gridValue
+})
+
+changeColor(gridValue);
 
 
